732edcb39db2accf2e0ad727a48d20ec5478206c
puppet <puppetdev@163.com>
2022-05-16 20:52:15 +0800

    912排序

--- Java/src/dev/puppet/leetcode/editor/cn/SortAnArray.java
+++ Java/src/dev/puppet/leetcode/editor/cn/SortAnArray.java
@@ -31,6 +31,10 @@
 
 package dev.puppet.leetcode.editor.cn;
 
+import org.omg.CORBA.RepositoryIdHelper;
+
+import java.util.PrimitiveIterator;
+
 public class SortAnArray {
     public static void main(String[] args) {
         Solution solution = new SortAnArray().new Solution();
@@ -38,8 +42,45 @@ public class SortAnArray {
     
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
+        /**
+         * @Tag 排序算法、快排
+         * @Solution
+         * @TimeComplexity O(NlogN)
+         * @SpaceComplexity O(n)
+         */
         public int[] sortArray(int[] nums) {
-            return null;
+            if (nums.length <= 1) return nums;
+            int start = 0;
+            int end = nums.length - 1;
+            process(nums, start, end);
+            return nums;
+        }
+        
+        private void process(int[] nums, int start, int end) {
+            if (start >= end) return;
+            
+            int pivot = nums[start];
+            int left = start, right = end;
+            while (left < right) {
+                while (left < right) {
+                    if (nums[right] < pivot) {
+                        nums[left] = nums[right];
+                        break;
+                    }
+                    right--;
+                }
+                while (left < right) {
+                    if (nums[left] > pivot) {
+                        nums[right] = nums[left];
+                        break;
+                    }
+                    left++;
+                }
+            }
+            // 注意需要将位置填回去
+            nums[left] = pivot;
+            process(nums, start, left - 1);
+            process(nums, left + 1, end);
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
