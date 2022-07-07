57614591823f7b4c36af162d23ef7030e97d53d2
puppet <puppetdev@163.com>
2022-07-08 00:45:51 +0800

    '_'

---
+++ Java/src/dev/puppet/leetcode/editor/cn/ReorderList.java
@@ -0,0 +1,48 @@
+package dev.puppet.leetcode.editor.cn;
+
+/**
+ * 题目：143 重排链表
+ * @author puppet
+ * @since 2022-07-08 00:42:47
+ */
+public class ReorderList {
+    public static void main(String[] args) {
+        Solution solution = new ReorderList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public void reorderList(ListNode head) {
+
+        }
+    }
+
+    //leetcode submit region end(Prohibit modification and deletion)
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
