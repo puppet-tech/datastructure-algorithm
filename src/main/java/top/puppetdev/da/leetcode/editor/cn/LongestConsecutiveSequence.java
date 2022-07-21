8addf4b9935a7099e294e7688cffe996afc010de
puppet <puppetdev@163.com>
2022-07-21 23:19:59 +0800

    '_'

---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/LongestConsecutiveSequence.java
@@ -0,0 +1,27 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+/**
+ * 题目：128 最长连续序列
+ * @author puppet
+ * @since 2022-07-21 23:19:35
+ */
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        Solution solution = new LongestConsecutiveSequence().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * @TimeComplexity
+     * @SpaceComplexity
+     * @Classification
+     * @Tag
+     */
+    class Solution {
+        public int longestConsecutive(int[] nums) {
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
