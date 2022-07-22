681f2f6b80573da01100fef8d61157993c47417c
puppet <puppetdev@163.com>
2022-07-22 13:06:58 +0800

    '_'

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/LongestConsecutiveSequence.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/LongestConsecutiveSequence.java
@@ -1,5 +1,8 @@
 package top.puppetdev.da.leetcode.editor.cn;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 题目：128 最长连续序列
  * @author puppet
@@ -12,14 +15,30 @@ public class LongestConsecutiveSequence {
     //leetcode submit region begin(Prohibit modification and deletion)
 
     /**
-     * @TimeComplexity
-     * @SpaceComplexity
-     * @Classification
+     * See LeetCode answer parsing
+     * @TimeComplexity N need careful consideration
+     * @SpaceComplexity N
+     * @Classification hashmap
      * @Tag
      */
     class Solution {
         public int longestConsecutive(int[] nums) {
-            return 0;
+            // put all elements of nums into the hashmap to reduce the time complexity of search
+            Set<Integer> integers = new HashSet<>();
+            for (int num : nums) integers.add(num);
+            // define a variable to record the answer
+            int ans = 0;
+            for (int i = 0; i < nums.length; i++) {
+                // the brush of magic!!! to reduce time complexity
+                if (integers.contains(nums[i] - 1)) continue;
+
+                int y = 1;
+                while (integers.contains(nums[i] + y)) {
+                    y++;
+                }
+                ans = Math.max(y, ans);
+            }
+            return ans;
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
