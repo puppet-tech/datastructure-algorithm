7ed2904bdb42a541bf749c4b052a7d51eca8cb56
puppet <puppetdev@163.com>
2022-07-28 14:59:36 +0800

    _

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -1,16 +1,16 @@
 package top.puppetdev.da.leetcode.editor.cn;
 
 /**
- * 题目：69 x 的平方根
+ * 题目：34 在排序数组中查找元素的第一个和最后一个位置
  * @author puppet
- * @since 2022-07-27 23:58:49
+ * @since 2022-07-28 14:59:04
  */
-public class Sqrtx {
+public class FindFirstAndLastPositionOfElementInSortedArray {
     public static void main(String[] args) {
-        Solution solution = new Sqrtx().new Solution();
+        Solution solution = new FindFirstAndLastPositionOfElementInSortedArray().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-
+    
     /**
      * @TimeComplexity
      * @SpaceComplexity
@@ -18,8 +18,8 @@ public class Sqrtx {
      * @Tag 二分查找
      */
     class Solution {
-        public int mySqrt(int x) {
-            return 0;
+        public int[] searchRange(int[] nums, int target) {
+            return new int[0];
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
