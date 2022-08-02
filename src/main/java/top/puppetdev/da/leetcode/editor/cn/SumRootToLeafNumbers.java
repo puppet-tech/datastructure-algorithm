dc90216a1f1fd0c877c49f9eb4637af538c79ae0
puppet <puppetdev@163.com>
2022-08-02 22:23:14 +0800

    '_'

---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/SumRootToLeafNumbers.java
@@ -0,0 +1,39 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+import top.puppetdev.da.leetcode.editor.cn.common.TreeNode;
+
+/**
+ * 题目：129 求根节点到叶节点数字之和
+ * @author puppet
+ * @since 2022-08-02 22:22:34
+ */
+public class SumRootToLeafNumbers {
+    public static void main(String[] args) {
+        Solution solution = new SumRootToLeafNumbers().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public int sumNumbers(TreeNode root) {
+            int ans = 0;
+            return ans;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
