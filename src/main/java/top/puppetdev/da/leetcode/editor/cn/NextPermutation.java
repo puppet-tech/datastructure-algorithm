5b462591ba90c641b1e37ead2c8472bbfc061868
puppet <puppetdev@163.com>
2022-08-05 09:59:42 +0800

    _

---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/NextPermutation.java
@@ -0,0 +1,20 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+/**
+ * 题目：31 下一个排列
+ * @author puppet
+ * @since 2022-08-05 09:59:29
+ */
+public class NextPermutation{
+    public static void main(String[] args) {
+        Solution solution = new NextPermutation().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void nextPermutation(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
