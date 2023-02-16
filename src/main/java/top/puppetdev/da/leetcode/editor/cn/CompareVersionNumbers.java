dd487e80e8226872a5bfe901e610062e077e480c
puppet <puppetdev@163.com>
2023-02-16 22:26:23 +0800

    Auto Pushed By Python Scripts at 2023-02-16 22:26:23

---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/CompareVersionNumbers.java
@@ -0,0 +1,21 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+/**
+ * 题目：165 比较版本号
+ * @author puppet
+ * @since 2023-02-16 22:26:03
+ */
+public class CompareVersionNumbers {
+    public static void main(String[] args) {
+        Solution solution = new CompareVersionNumbers().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int compareVersion(String version1, String version2) {
+            return 0;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
--- src/main/java/top/puppetdev/da/leetcode/editor/cn/NextPermutation.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/NextPermutation.java
@@ -5,16 +5,47 @@ package top.puppetdev.da.leetcode.editor.cn;
  * @author puppet
  * @since 2022-08-05 09:59:29
  */
-public class NextPermutation{
+public class NextPermutation {
     public static void main(String[] args) {
         Solution solution = new NextPermutation().new Solution();
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public void nextPermutation(int[] nums) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void nextPermutation(int[] nums) {
+            int len = nums.length;
+            if (len <= 1) return;
+            int i = len - 2, j = len - 1, k = len - 1;
+            while (i >= 0 && nums[j] <= nums[i]) {
+                j--;
+                i--;
+            }
+            if (i >= 0) {
+                while (nums[k] <= nums[i]) {
+                    k--;
+                }
+                // 交换 i 和 k 的位置
+                swap(nums, i, k);
+            }
+            // 将 [j, end) 后的都进行翻转
+            reverse(nums, j);
+        }
+
+        private void reverse(int[] nums, int start) {
+            int left = start, right = nums.length - 1;
+            while (left < right) {
+                swap(nums, left, right);
+                left++;
+                right--;
+            }
+        }
+
+        private void swap(int[] nums, int i, int k) {
+            int temp = nums[i];
+            nums[i] = nums[k];
+            nums[k] = temp;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
