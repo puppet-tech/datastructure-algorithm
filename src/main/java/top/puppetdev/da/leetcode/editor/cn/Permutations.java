8d9c503aa8e25e2b810a3e84db45e2a57c695f38
puppet <puppetdev@163.com>
2023-03-12 22:45:15 +0800

    Auto Pushed By Python Scripts at 2023-03-12 22:45:15

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/Permutations.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/Permutations.java
@@ -1,20 +1,23 @@
 package top.puppetdev.da.leetcode.editor.cn;
 
+import java.util.List;
+
 /**
  * 题目：46 全排列
  * @author puppet
  * @since 2023-03-07 23:09:50
  */
-public class Permutations{
+public class Permutations {
     public static void main(String[] args) {
         Solution solution = new Permutations().new Solution();
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> permute(int[] nums) {
+
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
