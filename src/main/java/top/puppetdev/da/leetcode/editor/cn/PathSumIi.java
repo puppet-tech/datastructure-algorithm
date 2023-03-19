a695366a794ab5330b024d738c4ecf3c9ad5cac8
puppet <puppetdev@163.com>
2023-03-20 00:37:53 +0800

    Auto Pushed By Python Scripts at 2023-03-20 00:37:53

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/PathSumIi.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/PathSumIi.java
@@ -1,35 +1,40 @@
 package top.puppetdev.da.leetcode.editor.cn;
 
+import top.puppetdev.da.leetcode.editor.cn.common.TreeNode;
+
+import java.util.List;
+
 /**
  * 题目：113 路径总和 II
  * @author puppet
  * @since 2023-03-16 00:04:47
  */
-public class PathSumIi{
+public class PathSumIi {
     public static void main(String[] args) {
         Solution solution = new PathSumIi().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
 
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
