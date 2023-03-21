7e94808dda9d87b9f467758b0d1ce7e5b5aede9e
puppet <puppetdev@163.com>
2023-03-22 02:11:29 +0800

    Auto Pushed By Python Scripts at 2023-03-22 02:11:28

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/GenerateParentheses.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/GenerateParentheses.java
@@ -1,20 +1,23 @@
 package top.puppetdev.da.leetcode.editor.cn;
 
+import java.util.List;
+
 /**
  * 题目：22 括号生成
  * @author puppet
  * @since 2023-03-16 00:15:38
  */
-public class GenerateParentheses{
+public class GenerateParentheses {
     public static void main(String[] args) {
         Solution solution = new GenerateParentheses().new Solution();
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public List<String> generateParenthesis(int n) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<String> generateParenthesis(int n) {
+
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
