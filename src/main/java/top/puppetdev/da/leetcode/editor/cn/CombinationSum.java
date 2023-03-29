52d85a60834ea0830ba80a225d3185b39e2a2007
puppet <puppetdev@163.com>
2023-03-29 18:44:58 +0800

    Auto Pushed By Python Scripts at 2023-03-29 18:44:58

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/CombinationSum.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/CombinationSum.java
@@ -20,8 +20,27 @@ public class CombinationSum {
         List<Integer> path = new ArrayList<>();
 
         public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
+            backtrack(candidates, target);
             return ret;
+        }
+
+        private void backtrack(int[] candidates, int target) {
+            for (int i = 0; i < candidates.length; i++) {
+                path.add(candidates[i]);
+                target -= candidates[i];
+                if (target == 0) {
+                    ret.add(new ArrayList<>(path));
+                    return;
+                }
+                if (target < 0) {
+                    path.remove(path.size() - 1);
+                    target += candidates[i];
+                    continue;
+                }
+                backtrack(candidates, target);
+                path.remove(path.size() - 1);
+                target += candidates[i];
+            }
         }
     }
 //leetcode submit region end(Prohibit modification and deletion)
