e74fa4546a69125fd47bd397f3e5f05df1a71234
puppet <puppetdev@163.com>
2023-04-04 19:07:20 +0800

    Auto Pushed By Python Scripts at 2023-04-04 19:07:20

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/ThreeSum.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/ThreeSum.java
@@ -13,6 +13,8 @@ import java.util.List;
 public class ThreeSum {
     public static void main(String[] args) {
         Solution solution = new ThreeSum().new Solution();
+        int[] nums = new int[]{0, 0,0,0};
+        solution.threeSum(nums);
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -26,19 +28,37 @@ public class ThreeSum {
      */
     class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
-            if (nums == null || nums.length < 3) return new ArrayList<>(0);
+            if (nums == null || nums.length < 3) return List.of();
 
             List<List<Integer>> ans = new ArrayList<>();
+            if (nums.length == 3 && Arrays.stream(nums).sum() == 0) {
+                ans.add(List.of(nums[0], nums[1], nums[2]));
+                return ans;
+            }
             Arrays.sort(nums);
 
             for (int i = 0; i < nums.length; i++) {
-                if (nums[i] >= 0) break;
+                if (nums[i] > 0) break;
                 if (i != 0 && nums[i - 1] == nums[i]) continue;
                 int lp = i + 1, rp = nums.length - 1;
-                while (lp != rp) {
-                    if (nums[i] + nums[lp] + nums[rp] < 0) lp++;
-                    if (nums[i] + nums[lp] + nums[rp] > 0) rp--;
-                    if (nums[i] + nums[lp] + nums[rp] == 0) ans.add(List.of(nums[i], nums[lp], nums[rp]));
+                while (lp < rp && nums[i] + nums[lp] + nums[rp] < 0) {
+                        lp++;
+                }
+                while (lp < rp && nums[i] + nums[lp] + nums[rp] > 0) {
+                        rp--;
+                }
+                if (nums[i] + nums[lp] + nums[rp] == 0) {
+                        if (nums[lp] == nums[lp + 1]) {
+                            lp++;
+                            continue;
+                        }
+                        if (nums[rp] == nums[rp - 1]) {
+                            rp--;
+                            continue;
+                        }
+                        ans.add(List.of(nums[i], nums[lp], nums[rp]));
+                        lp++;
+                    }
                 }
             }
             return ans;
