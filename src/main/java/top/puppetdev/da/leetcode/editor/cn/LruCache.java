5d3ba3dec920364149ddfd2e2ee60ab54180b990
puppet <puppetdev@163.com>
2023-04-09 02:09:14 +0800

    Auto Pushed By Python Scripts at 2023-04-09 02:09:14

---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/LruCache.java
@@ -0,0 +1,36 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+/**
+ * 题目：146 LRU 缓存
+ * @author puppet
+ * @since 2023-04-09 02:08:53
+ */
+public class LruCache{
+    public static void main(String[] args) {
+        Solution solution = new LruCache().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class LRUCache {
+
+    public LRUCache(int capacity) {
+
+    }
+    
+    public int get(int key) {
+
+    }
+    
+    public void put(int key, int value) {
+
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
