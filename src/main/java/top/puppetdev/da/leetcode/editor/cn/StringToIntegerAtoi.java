cf94c329232e40915f915c2f1496c13bacf26a7c
puppet <puppetdev@163.com>
2023-04-14 16:16:13 +0800

    Auto Pushed By Python Scripts at 2023-04-14 16:16:13

---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/StringToIntegerAtoi.java
@@ -0,0 +1,20 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+/**
+ * 题目：8 字符串转换整数 (atoi)
+ * @author puppet
+ * @since 2023-04-14 16:16:05
+ */
+public class StringToIntegerAtoi{
+    public static void main(String[] args) {
+        Solution solution = new StringToIntegerAtoi().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int myAtoi(String s) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
