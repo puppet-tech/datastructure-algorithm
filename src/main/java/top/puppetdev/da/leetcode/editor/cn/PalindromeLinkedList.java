9563aa649ce9bcd4eb3689fa31a8fdfaae6521f2
puppet <puppetdev@163.com>
2023-04-18 20:24:41 +0800

    Auto Pushed By Python Scripts at 2023-04-18 20:24:41

---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/PalindromeLinkedList.java
@@ -0,0 +1,30 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+/**
+ * 题目：234 回文链表
+ * @author puppet
+ * @since 2023-04-18 12:37:57
+ */
+public class PalindromeLinkedList{
+    public static void main(String[] args) {
+        Solution solution = new PalindromeLinkedList().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
