87189c00a5b355b2044081a7113693a68a401c59
puppet <puppetdev@163.com>
2023-04-20 01:00:48 +0800

    Auto Pushed By Python Scripts at 2023-04-20 01:00:48

--- src/main/java/top/puppetdev/da/leetcode/editor/cn/BinarySearch.java
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/BinarySearch.java
@@ -2,19 +2,29 @@ package top.puppetdev.da.leetcode.editor.cn;
 
 /**
  * 题目：704 二分查找
+ *
  * @author puppet
  * @since 2023-04-18 23:34:49
  */
-public class BinarySearch{
+public class BinarySearch {
     public static void main(String[] args) {
         Solution solution = new BinarySearch().new Solution();
     }
-    //leetcode submit region begin(Prohibit modification and deletion)
-class Solution {
-    public int search(int[] nums, int target) {
 
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int search(int[] nums, int target) {
+            int start = 0, end = nums.length - 1;
+            while (start <= end) {
+//                int mid = (start + end) >> 1;
+                int mid = start + ((end - start) >> 1);
+                if (nums[mid] == target) return mid;
+                if (nums[mid] < target) start = mid + 1;
+                if (nums[mid] > target) end = mid - 1;
+            }
+            return -1;
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
---
+++ src/main/java/top/puppetdev/da/leetcode/editor/cn/FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,20 @@
+package top.puppetdev.da.leetcode.editor.cn;
+
+/**
+ * 题目：1160 拼写单词
+ * @author puppet
+ * @since 2023-04-20 00:59:15
+ */
+public class FindWordsThatCanBeFormedByCharacters{
+    public static void main(String[] args) {
+        Solution solution = new FindWordsThatCanBeFormedByCharacters().new Solution();
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int countCharacters(String[] words, String chars) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
